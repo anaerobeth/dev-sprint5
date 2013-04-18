1b9b9ca2cd75cb011f185e4129c10374b44fbcdf
Elizabeth Tenorio <betsky98@gmail.com>
2013-04-18 01:50:14 -0400

    Add comments

---
+++ chap12.py
@@ -0,0 +1,18 @@
+#This is where the answers to Chapter 12 questions for the BSS Dev RampUp go
+# Name: Elizabeth Tenorio
+
+#Ex. 12.4
+#1. Write a program that reads a word list from a file (see Section 9.1)
+# and prints all the sets of words that are anagrams
+
+
+#2. Modify the previous program so that it prints the largest set of anagrams first,
+# followed by the second largest set, and so on.
+
+
+#3. In Scrabble a “bingo” is when you play all seven tiles in your rack,
+# along with a letter on the board, to form an eight-letter word.
+# What set of 8 letters forms the most possible bingos? Hint: there are seven.
+
+
+
---
+++ chap13.py
@@ -0,0 +1,16 @@
+#This is where the answers to Chapter 13 questions for the BSS Dev RampUp go
+# Name: Elizabeth Tenorio
+
+#1. Write a program to read a text from a file and perform Markov analysis.
+# The result should be a dictionary that maps from prefixes to a collection of possible suffixes.
+
+
+
+#2. Add a function to the previous program to generate random text based
+#on the Markov analysis.
+
+
+
+#3. 3. Once your program is working, you might want to try a mash-up:
+# if you analyze text from two or more books, the random text you generate will
+#blend the vocabulary and phrases from the sources in interesting ways
