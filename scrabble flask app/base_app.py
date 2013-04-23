1c772530c5455c04b9cc0d2faac219a4546e52a7
Elizabeth Tenorio <betsky98@gmail.com>
2013-04-23 01:53:46 -0400

    Trim down to basic_app

--- scrabble flask app/base_app.py
+++ scrabble flask app/base_app.py
@@ -1,4 +1,4 @@
-# Week3 web app
+# Scrabble app
 # Name: Elizabeth Tenorio
 
 import flask
@@ -7,10 +7,6 @@ import settings
 #Views
 from login import Login
 from main import Main
-from remote import Remote
-from music import Music
-from reverse import Reverse
-from gallery import Gallery
 
 app = flask.Flask(__name__)
 app.secret_key = settings.secret_key
@@ -23,21 +19,7 @@ app.add_url_rule('/',
 app.add_url_rule('/<page>/',
                 view_func=Main.as_view('main'),
                 methods=['GET'])
-app.add_url_rule('/gallery/',
-                view_func=Gallery.as_view('gallery'),
-                methods=['GET'])
-app.add_url_rule('/login/',
-                view_func=Login.as_view('login'),
-                methods=['GET', 'POST'])
-app.add_url_rule('/remote/',
-                view_func=Remote.as_view('remote'),
-                methods=['GET', 'POST'])
-app.add_url_rule('/reverse/',
-                view_func=Reverse.as_view('reverse'),
-                methods=['GET', 'POST'])
-app.add_url_rule('/music/',
-                view_func=Music.as_view('music'),
-                methods=['GET'])
+
 
 @app.errorhandler(404)
 def page_not_found(error):
