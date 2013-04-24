7dcf6a58ef05cf49418ace493f8b80edbcd45651
Elizabeth Tenorio <betsky98@gmail.com>
2013-04-24 00:52:51 -0400

    Add styling to basic_app

---
+++ scrabble flask app/basic_app.py
@@ -0,0 +1,28 @@
+# Scrabble app
+# Name: Elizabeth Tenorio
+
+import flask
+import settings
+
+#Views
+from main import Main
+
+app = flask.Flask(__name__)
+app.secret_key = settings.secret_key
+
+
+#Routes
+app.add_url_rule('/',
+                view_func=Main.as_view('main'),
+                methods=['GET'])
+app.add_url_rule('/<page>/',
+                view_func=Main.as_view('main'),
+                methods=['GET'])
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+     return flask.render_template('404.html'), 404
+
+app.debug = True
+app.run()
